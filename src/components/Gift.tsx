7a97ff199141372a5c4a0332c439b74c3ee0f24d
Erick <batista.erick@outlook.com>
2022-08-24 16:17:34 -0300

    Creating the gift

---
+++ src/components/Gift.tsx
@@ -0,0 +1,14 @@
+import styles from "../styles/Gift.module.css";
+
+const Gift = () => {
+  return (
+    <div className={styles.gift}>
+      <div className={styles.lid}></div>
+      <div className={styles.body}></div>
+      <div className={styles.giftBowHorizontal}></div>
+      <div className={styles.giftBowVertical}></div>
+    </div>
+  );
+};
+
+export default Gift;
---
+++ src/pages/index.tsx
@@ -0,0 +1,12 @@
+import type { NextPage } from "next";
+import Gift from "../components/Gift";
+
+const Home: NextPage = () => {
+  return (
+    <div>
+      <Gift />
+    </div>
+  );
+};
+
+export default Home;
