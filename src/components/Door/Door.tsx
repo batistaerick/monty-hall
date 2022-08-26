df847f191ab455fb27d23c36ba4d9255aa9f696e
Erick <batista.erick@outlook.com>
2022-08-26 17:30:22 -0300

    Making the door handle work

--- src/components/Door/Door.tsx
+++ src/components/Door/Door.tsx
@@ -8,19 +8,31 @@ interface DoorProps {
 }
 
 const Door: FC<DoorProps> = ({ doorModel, onChange }) => {
-  const selectedStyle = doorModel.selected ? styles.selected : "";
+  const selectedStyle =
+    doorModel.selected && !doorModel.opened ? styles.selected : "";
 
   const changeSelection = () => {
     onChange(doorModel.changeSelection());
   };
 
+  const openTheDoor = (event: any | undefined) => {
+    event?.stopPropagation();
+    onChange(doorModel.open());
+  };
+
+  const doorRender = () => {
+    return (
+      <div className={styles.door}>
+        <div className={styles.number}>{doorModel.number}</div>
+        <div className={styles.doorHandle} onClick={openTheDoor}></div>
+      </div>
+    );
+  };
+
   return (
     <div className={styles.area} onClick={changeSelection}>
       <div className={`${styles.frame} ${selectedStyle}`}>
-        <div className={styles.door}>
-          <div className={styles.number}>{doorModel.number}</div>
-          <div className={styles.doorHandle}></div>
-        </div>
+        {doorModel.opened ? false : doorRender()}
       </div>
       <div className={styles.floor}></div>
     </div>
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -5,15 +5,9 @@ import DoorModel from "../models/DoorModel";
 
 const Home: NextPage = () => {
   const [door, setDoor] = useState<DoorModel>(new DoorModel(1));
-  const [text, setText] = useState<string>();
 
   return (
     <div style={{ display: "flex", flexDirection: "column" }}>
-      <input
-        type="text"
-        value={text}
-        onChange={(event) => setText(event.target.value)}
-      />
       <Door doorModel={door} onChange={setDoor} />
     </div>
   );
