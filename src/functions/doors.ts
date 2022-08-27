5be8a4a4ba8fd5c5e164845be51891d9c5fbaf3f
Erick <batista.erick@outlook.com>
2022-08-27 17:28:14 -0300

    Door array implementation

---
+++ src/functions/doors.ts
@@ -0,0 +1,20 @@
+import DoorModel from "../models/DoorModel";
+
+function createDoors(quantity: number, selected: number): DoorModel[] {
+  return Array.from({ length: quantity }, (_, index) => {
+    const number: number = index + 1;
+    return new DoorModel(number, number === selected);
+  });
+}
+
+function updateDoors(doors: DoorModel[], modifiedDoor: DoorModel): DoorModel[] {
+  return doors.map((door) => {
+    if (door.number === modifiedDoor.number) {
+      return modifiedDoor;
+    } else {
+      return modifiedDoor.opened ? door : door.deselect();
+    }
+  });
+}
+
+export { createDoors, updateDoors };
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,16 +1,23 @@
 import type { NextPage } from "next";
 import { useState } from "react";
 import Door from "../components/Door/Door";
+import { createDoors, updateDoors } from "../functions/doors";
 import DoorModel from "../models/DoorModel";
 
 const Home: NextPage = () => {
-  const [door, setDoor] = useState<DoorModel>(new DoorModel(1));
+  const [doors, setDoors] = useState<DoorModel[]>(createDoors(4, 1));
 
-  return (
-    <div style={{ display: "flex", flexDirection: "column" }}>
-      <Door doorModel={door} onChange={setDoor} />
-    </div>
-  );
+  const doorRender = () => {
+    return doors.map((door) => (
+      <Door
+        key={door.number}
+        doorModel={door}
+        onChange={(newDoor) => setDoors(updateDoors(doors, newDoor))}
+      />
+    ));
+  };
+
+  return <div style={{ display: "flex" }}>{doorRender()}</div>;
 };
 
 export default Home;
